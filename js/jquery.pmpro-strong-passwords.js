1176f884dfd3c2e00ab37daef354a848bdfb5819
Theuns Coetzee <ipokkel@gmail.com>
2019-07-29 08:43:21 +0200

    add password strength indicator

---
+++ js/jquery.pmpro-strong-passwords.js
@@ -0,0 +1,60 @@
+function checkPasswordStrength( 
+	$password_field_1,
+    $password_field_2,
+	$strengthResult
+	 ) {
+
+    var password_field_1 = $password_field_1.val();
+    var password_field_2 = $password_field_2.val();
+ 
+    // Reset the form & meter
+	$strengthResult.removeClass( 'short bad good strong' );
+ 
+    // Get the password strength
+    var strength = wp.passwordStrength.meter( password_field_1, password_field_2 );
+ 
+    // Add the strength meter results
+    switch ( strength ) {
+        case 2:
+            $strengthResult.addClass( 'bad' ).html( pwsL10n.bad );
+            break;
+
+        case 3:
+            $strengthResult.addClass( 'good' ).html( pwsL10n.good );
+            break;
+ 
+        case 4:
+            $strengthResult.addClass( 'strong' ).html( pwsL10n.strong );
+            break;
+ 
+        case 5:
+            $strengthResult.addClass( 'short' ).html( pwsL10n.mismatch );
+            break;
+ 
+        default:
+            $strengthResult.addClass( 'short' ).html( pwsL10n.short );
+ 
+    }
+ 
+    // The meter function returns a result even if password_field_2 is empty,
+    // enable only the submit button if the password is strong and
+    // both passwords are filled up
+    if ( 4 === strength && '' !== password_field_2.trim() ) {
+        $submitButton.removeAttr( 'disabled' );
+    }
+ 
+    return strength;
+}
+ 
+jQuery( document ).ready( function( $ ) {
+    // Binding to trigger checkPasswordStrength
+    jQuery( 'body' ).on( 'keyup', 'input[name=password], input[name=password2]',
+        function( event ) {
+            checkPasswordStrength(
+                jQuery('input[name=password]'),         // First password field
+                jQuery('input[name=password2]'), // Second password field
+                jQuery('#password-strength')           // Strength meter
+            );
+        }
+    );
+});
--- pmpro-strong-passwords.php
+++ pmpro-strong-passwords.php
@@ -18,7 +18,37 @@ Domain Path: /languages
 function pmprosp_load_plugin_text_domain() {
 	load_plugin_textdomain( 'pmpro-strong-passwords', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
-add_action( 'init', 'pmprosp_load_plugin_text_domain' ); 
+add_action( 'init', 'pmprosp_load_plugin_text_domain' );
+
+function pmprosp_password_strength_scripts_and_styles() {
+	wp_enqueue_script( 'password-strength-meter' );
+	wp_register_script( 'pmprosp', plugins_url( 'js/jquery.pmpro-strong-passwords.js', __FILE__ ), array( 'jquery' ), false, true );
+
+	$testname   = __( 'test name', 'pmpro' );
+	$testnoname = __( 'test no name' );
+
+	global $pmpro_pages;
+	// Only load on checkout page
+	if ( is_page( $pmpro_pages['checkout'] ) ) {
+		wp_enqueue_script( 'pmprosp' );
+		wp_enqueue_style( 'pmprosp', plugins_url( 'css/pmpro-strong-passwords.css', __FILE__ ) );
+	}
+
+	wp_localize_script(
+		'password-strength-meter',
+		'pwsL10n',
+		array(
+			'empty'    => _x( 'Strength indicator', 'password strength', 'pmpro-strong-passwords' ),
+			'short'    => _x( 'Very weak', 'password strength', 'pmpro-strong-passwords' ),
+			'bad'      => _x( 'Weak', 'password strength', 'pmpro-strong-passwords' ),
+			'good'     => _x( 'Medium', 'password strength', 'pmpro-strong-passwords' ),
+			'strong'   => _x( 'Strong', 'password strength', 'pmpro-strong-passwords' ),
+			'mismatch' => _x( 'Mismatch', 'password strength', 'pmpro-strong-passwords' ),
+		)
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'pmprosp_password_strength_scripts_and_styles' );
 
 /**
  * This function checks to make sure the user has submitted a strong password
@@ -87,7 +117,9 @@ add_filter( 'pmpro_registration_checks', 'pmpro_strong_password_check' );
 function pmprosp_pmpro_checkout_after_password()
 {
 ?>
+<span id="password-strength"></span>
 <p><?php _e( 'Note: Your password must be at least 8 characters long and contain upper and lowercase letters, a number, and a special character.', 'pmpro-strong-passwords' ) ?></p>
 <?php
 }
-add_filter("pmpro_checkout_after_password", "pmprosp_pmpro_checkout_after_password");
+// load as early as possible in case there are uses of filter
+add_filter( 'pmpro_checkout_after_password', 'pmprosp_pmpro_checkout_after_password', 1, 1 );
